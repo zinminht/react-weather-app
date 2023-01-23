28589ed790ab2e6697ba088e3a5f85565360306e
zinminht <xioniven@gmail.com>
2023-01-23 14:58:30 +0630

    23 Jan 2023

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '69fdf7604emsh919911b5f06d23dp1e454bjsn16d4231d399b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9e63024dbcf626fdc20cc1f82711d27f";
